94c5dba21e867d137f1d95f127dff47c1a121a61
atulgholap8 <atulgholap8@gmail.com>
2023-06-05 10:14:30 +0530

    commite

--- DveloperProject/test/com/CalculatorTest/CalculatorTest.java
+++ DveloperProject/test/com/CalculatorTest/CalculatorTest.java
@@ -11,21 +11,21 @@ public class CalculatorTest {
 	@Test
 	public void addTesting()
 	{
-				assertEquals(obj.addition(7, 7), 14);
+		assertEquals(obj.addition(7, 7), 14);
 	}
 	@Test
 	public void subTesting()
 	{
-				assertEquals(obj.substarction(7, 5), 2);
+		assertEquals(obj.substarction(7, 5), 2);
 	}
 	@Test
 	public void mulTesting()
 	{
-				assertEquals(obj.multiplication(7, 5), 35);
+		assertEquals(obj.multiplication(7, 5), 35);
 	}
 	@Test
 	public void divTesting()
 	{
-				assertEquals(obj.division(10, 0), 2);
+		assertEquals(obj.division(10, 0), 2);
 	}
 }
